f3915d77026d5bd83abe00d19ed20646f07e67b6
unknown <S_Gravesen92@hotmail.com>
2018-05-30 17:58:33 +0200

    Removed old text from PortfolioPanel

--- src/components/PortfolioPanel.js
+++ src/components/PortfolioPanel.js
@@ -19,7 +19,6 @@ const PortfolioPanel = ({ showcaseProjects, openPortfolio }) => {
                         <p>As a passionate web developer, I have made a bunch of personal projects.</p>
                         <p>Most projects are front-end based, but some also takes advantage of some API's, like the weather app.</p>
                         <p>A few of the project are also made during collaboration with other developers</p>
-                        <p>-- Add more text</p>
                     </div>
                     <a onClick={openPortfolio} href="#">View All Projects</a>
                     </div>
