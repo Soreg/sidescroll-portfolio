564054649fd5433463a7c4f6eeb190bd00ee6059
unknown <S_Gravesen92@hotmail.com>
2018-06-08 19:17:39 +0200

    Styled basic navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,22 +8,29 @@ const Navigation = () => {
                     <li>
                         <a href="#" data-panel="1">
                             <p>Welcome</p>
-                            <div className="nav-point"></div>
+                            <div className="nav-point">
+                                <div className="bullet"></div>
+                            </div>
                         </a>
                     </li>
                     <li>
                         <a href="#" data-panel="2">
                             <p>Portfolio</p>
-                            <div className="nav-point"></div>
+                            <div className="nav-point">
+                                <div className="bullet"></div>
+                            </div>
                         </a>
                     </li>
                     <li>
                         <a href="#" data-panel="3">
                             <p>Contact</p>
-                            <div className="nav-point"></div>
+                            <div className="nav-point">
+                                <div className="bullet"></div>
+                            </div>
                         </a>
                     </li>
                 </ul>
+                <div className="line"></div>
             </nav>
         </div>
     )   
