7efdfc01bf8670d3db2588a13f9868f317136be7
unknown <S_Gravesen92@hotmail.com>
2018-06-08 18:27:45 +0200

    Added basic syntax for Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 const Navigation = () => {
     return(
-        <div className="Navigation"></div>
+        <div className="Navigation">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#" data-panel="1">
+                            <p>Welcome</p>
+                            <div className="nav-point"></div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" data-panel="2">
+                            <p>Portfolio</p>
+                            <div className="nav-point"></div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" data-panel="3">
+                            <p>Contact</p>
+                            <div className="nav-point"></div>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )   
 }
 
